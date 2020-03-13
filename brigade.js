30384fc276225cda283b9f77bea3b1f28042215f
Valery Masiutsin <val.masutin@gmail.com>
2020-03-13 11:01:39 +0000

    Adding brigade.js file

---
+++ brigade.js
@@ -0,0 +1,9 @@
+const devops = require("devops-brigade");
+devops.Events.onPushDevelop(async () => {
+  await devops.Utilities.gitVersionAsync();
+  await devops.Standard.publishHelmChartAsync({ 
+    chartName: "brigade", 
+    helmSubFolder: "./",
+    getDependencies: false
+  });
+});
